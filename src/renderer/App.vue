420064bf1c6ea12b2cd7fffa1929e48fb6561cbe
Joowce <minjw1026@naver.com>
2019-12-04 22:12:32 +0900

    initialize project w/ electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tinkerbell-front'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
