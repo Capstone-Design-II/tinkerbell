269985dce3a4d3b669a8fcc8c11c63d2054f0a1c
Joowce <minjw1026@naver.com>
2019-12-22 02:33:46 +0900

    Renderer: Add menu item component

---
+++ src/renderer/components/menu/BaseMenuItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <a :class="['mdc-list-item', isActive ? 'mdc-list-item--activated': '']" :href='href' aria-current="page">
+        <slot name="icon"></slot>
+        <span class="mdc-list-item__text">{{ text }}</span>
+    </a>
+</template>
+
+<script>
+  export default {
+    name: 'BaseMenuItem',
+    props: {
+      isActive: {
+        type: Boolean,
+        default: false
+      },
+      href: {
+        type: String,
+        default: '#'
+      },
+      text: {
+        type: String,
+        default: 'Menu'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
