424254989433fa9e71e20b2ce3eabf897001502d
Joowce <minjw1026@naver.com>
2019-12-22 04:42:33 +0900

    Renderer: BaseDivider component

---
+++ src/renderer/components/base/BaseDivider.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="base-divider"></div>
+</template>
+
+<script>
+  export default {
+    name: 'BaseDivider'
+  }
+</script>
+
+<style scoped>
+.base-divider {
+    height: 1px;
+    background-color: rgba(0, 0, 0, 0.12);
+}
+</style>
