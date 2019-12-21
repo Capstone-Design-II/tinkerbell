3a2a46a4f5951e1f6edc8458423f4d53231be360
Joowce <minjw1026@naver.com>
2019-12-22 02:34:27 +0900

    Renderer: Add menu profile

---
+++ src/renderer/components/menu/BaseProfile.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="base-profile">
+        <div class='base-profile__image'>
+            <img :src="imageSource" class="l-width-full">
+        </div>
+        <h3 class="mdc-drawer__title">{{ title }}</h3>
+        <h6 class="mdc-drawer__subtitle">
+            {{ subTitle }}
+            <span name="sub-menu"></span>
+        </h6>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'BaseProfile',
+    props: {
+      imageSource: {
+        type: String,
+        default: 'static/images/peterpan-128.png'
+      },
+      title: {
+        type: String,
+        default: 'Tinkerbell'
+      },
+      subTitle: {
+        type: String,
+        default: 'made by Peterpan'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .base-profile {
+        padding-top: 22px;
+    }
+.base-profile__image{
+    width: 48px;
+    height: 48px;
+    border-radius: 50%
+}
+</style>
