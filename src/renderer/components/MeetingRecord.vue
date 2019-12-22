618de18101c32f76079432bde8da42909b860b71
Joowce <minjw1026@naver.com>
2019-12-22 19:30:36 +0900

    Renderer: Add Meeting Record.vue

---
+++ src/renderer/components/MeetingRecord.vue
@@ -0,0 +1,28 @@
+<template>
+    <base-file-input title="Record Meeting" default-description="Select meeting record"
+                     @file:submit="submit">
+        <template v-slot:button-icon>
+            <i class="material-icons meeting-record-button__icon">note_add</i>
+        </template>
+    </base-file-input>
+</template>
+
+<script>
+  import BaseFileInput from './base/BaseFileInput'
+export default {
+    name: 'MeetingRecord',
+    components: {BaseFileInput},
+    methods: {
+      submit (file) {
+        console.log('MeetingNote')
+        console.log(file)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.meeting-record-button__icon {
+    font-size: 32px;
+}
+</style>
