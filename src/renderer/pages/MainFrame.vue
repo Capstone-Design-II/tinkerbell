e74e4efe3468b20ac57abcaf7139edf5f5a822bf
Joowce <minjw1026@naver.com>
2019-12-22 20:13:47 +0900

    Renderer: Change MainFrame layout

--- src/renderer/pages/MainFrame.vue
+++ src/renderer/pages/MainFrame.vue
@@ -2,7 +2,7 @@
     <div class="main-frame-root">
         <the-menu></the-menu>
         <div class="main-frame-app-content l-position-relative">
-            <top-bar class="mdc-top-app-bar__section--align-start"></top-bar>
+            <top-bar class="main-frame__top-bar mdc-top-app-bar__section--align-start" :title="title"></top-bar>
             <div class="main-frame-app__main-content">
                 <div class="mdc-top-app-bar--fixed-adjust"></div>
                 <router-view></router-view>
@@ -12,12 +12,24 @@
 </template>
 
 <script>
+  import * as R from 'ramda'
   import TheMenu from '../components/menu/TheMenu'
   import TopBar from '../components/TopBar'
 
   export default {
     name: 'MainFrame',
-    components: {TopBar, TheMenu}
+    components: {TopBar, TheMenu},
+    computed: {
+      title () {
+        return R.ifElse(
+          R.isNil,
+          R.always(''),
+          R.pipe(
+            R.adjust(0, R.toUpper),
+            R.join('')
+          ))(this.$route.name)
+      }
+    }
   }
 </script>
 
@@ -25,6 +37,9 @@
     .main-frame-root {
         display: flex;
         height: 100vh;
+    }
+    .main-frame__top-bar {
+        position: absolute;
     }
 
     .main-frame-app-content {
