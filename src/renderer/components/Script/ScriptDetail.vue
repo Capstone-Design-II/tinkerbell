73702067ce92d2fafdff5976d0614f54674656b6
inkoon <md98765@naver.com>
2019-12-22 19:29:39 +0900

    Add script components.

---
+++ src/renderer/components/Script/ScriptDetail.vue
@@ -0,0 +1,24 @@
+<template>
+        <li class="mdc-list-item" tabindex="0">
+            <span class="mdc-list-item__text">
+                <span class="mdc-typography--subtitle1">{{items.title}}</span>
+                <span class="mdc-list-item__secondary-text">{{items.desc}}</span>
+            </span>
+        </li>
+</template>
+
+<script>
+    export default {
+      name: 'ScriptDetail',
+      props: {
+        items: {
+          type: Array,
+          default: []
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
